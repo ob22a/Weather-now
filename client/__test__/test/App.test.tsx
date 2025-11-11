489ac2fe2baec34174e654bd8b0ba06f72e457bc
Ob22a <Ob22adegefu123@gmail.com>
2025-11-11 18:59:11 +0300

    Valid and working test

--- client/__test__/test/App.test.tsx
+++ client/__test__/test/App.test.tsx
@@ -7,7 +7,7 @@ describe("App component", () => {
   it("renders the greeting message", () => {
     render(<App />);
     // Look for the text in the document
-    const heading = screen.getByText(/hello react/i);
+    const heading = screen.getByText(/hello Weather now user/i);
     expect(heading).toBeDefined();
   });
 });
--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,7 @@
 function App() {
   return (
     <div>
-      <h1>Hello React</h1>
+      <h1>Hello Weather now user</h1>
       <p>Welcome to my app!</p>
     </div>
   );
