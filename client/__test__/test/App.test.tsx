99cfb4763540f6d3b2f9078135efb300f119a850
Ob22a <Ob22adegefu123@gmail.com>
2025-11-11 19:01:00 +0300

    test works

--- client/__test__/test/App.test.tsx
+++ client/__test__/test/App.test.tsx
@@ -7,7 +7,7 @@ describe("App component", () => {
   it("renders the greeting message", () => {
     render(<App />);
     // Look for the text in the document
-    const heading = screen.getByText(/hello Weather now user/i);
+    const heading = screen.getByText(/Weather now user/i);
     expect(heading).toBeDefined();
   });
 });
