35e87b16550ce4a057ab05dfae6be4554c439d08
Ob22a <Ob22adegefu123@gmail.com>
2025-11-11 18:05:34 +0300

    Testing branch made to test the workflow

--- client/__test__/test/App.test.tsx
+++ client/__test__/test/App.test.tsx
@@ -7,7 +7,7 @@ describe("App component", () => {
   it("renders the greeting message", () => {
     render(<App />);
     // Look for the text in the document
-    const heading = screen.getByText(/hello react/i);
+    const heading = screen.getByText(/hello Weather Now User/i);
     expect(heading).toBeDefined();
   });
 });
